fbdd6068f6bb8393ccc4bf7b02aa4c082c093fee
Tarunbhargava <164621051+Tarunbhargava@users.noreply.github.com>
2024-08-09 17:25:10 +0530

    Add files via upload

---
+++ em-project/src/test/java/com/crudproject/em_project/EmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudproject.em_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
